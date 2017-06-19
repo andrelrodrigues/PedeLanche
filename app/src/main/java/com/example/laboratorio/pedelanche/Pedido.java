28fb28f72c68d0c02b1069ecb5b421b26920502e
André Rodrigues <by.andrerodrigues@gmail.com>
2017-06-19 18:10:20 -0300

    Alterado ImagemButton para ImageView no Layout Item_menu Criado função caixaAlerta na Classe Manu, para perguntar se usuário quer finalizar pedido ou adicionar mais itens para compra. Implementado setOnItemclickListener na classe Menu para pegar produtos selecionados pelo usuário adicionar ao ArrayList produtoPedido

--- app/src/main/java/com/example/laboratorio/pedelanche/Pedido.java
+++ app/src/main/java/com/example/laboratorio/pedelanche/Pedido.java
@@ -3,15 +3,34 @@ package com.example.laboratorio.pedelanche;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
+import com.example.laboratorio.pedelanche.model.Produto;
+
+import java.util.ArrayList;
 
 /**
  * Created by laboratorio on 18/05/17.
  */
 
 public class Pedido extends AppCompatActivity{
+    public static ArrayList<Produto> produtoPedido = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pedido);
+
+        ArrayList<Produto> proutosPedido = Pedido.produtoPedido;
+
+        Integer tamnanho = proutosPedido.size();
+        Log.d("Tamanho", tamnanho.toString());
+
+        for(int i=0;  i < proutosPedido.size();i++){
+            Produto produto = proutosPedido.get(i);
+            Log.d("Pedido ", produto.getProduto() + " " + produto.getValor());
+        }
+
     }
 }
