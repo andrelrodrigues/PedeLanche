ace3d83da0f63a6e15d6582e09f0234ad1d24053
André Rodrigues <by.andrerodrigues@gmail.com>
2017-06-20 20:42:08 -0300

    Realizado ajustes e implementações na classe Pedido, Layout pedido e criado Layout item_pedido.

--- app/src/main/java/com/example/laboratorio/pedelanche/Pedido.java
+++ app/src/main/java/com/example/laboratorio/pedelanche/Pedido.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.example.laboratorio.pedelanche.adapter.ProdutosAdapter;
 import com.example.laboratorio.pedelanche.model.Produto;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
 
 /**
@@ -15,22 +19,34 @@ import java.util.ArrayList;
  */
 
 public class Pedido extends AppCompatActivity{
+    NumberFormat formato = NumberFormat.getCurrencyInstance();
     public static ArrayList<Produto> produtoPedido = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pedido);
+        final ListView listaPedido = (ListView) findViewById(R.id.list_pedido);
+        TextView valorTotal = (TextView) findViewById(R.id.tvValorTotal);
 
         ArrayList<Produto> proutosPedido = Pedido.produtoPedido;
+
+        double totalPedido = 0.0;
+
+        ProdutosAdapter adapter = new ProdutosAdapter(this, proutosPedido);
+        listaPedido.setAdapter(adapter);
 
         Integer tamnanho = proutosPedido.size();
         Log.d("Tamanho", tamnanho.toString());
 
         for(int i=0;  i < proutosPedido.size();i++){
             Produto produto = proutosPedido.get(i);
-            Log.d("Pedido ", produto.getProduto() + " " + produto.getValor());
+            totalPedido = totalPedido + produto.getValor();
+            Log.d("Pedido ", produto.getProduto() + " " + produto.getValor() + " soma: " + valorTotal);
         }
+
+        valorTotal.setText(formato.format(totalPedido));
+
 
     }
 }
